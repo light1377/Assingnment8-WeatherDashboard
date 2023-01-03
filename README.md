f26a416cc55b9c00cb870f90ff2009fc8aaca36f
Moral <moral@meshkot.com>
2023-01-03 01:19:05 +0000

    added to README, changed heading colour, tried to make ajax work in javascript

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Description:This project's aim was to produce a 5-day weather app on the browser
 The app uses data from the https://home.openweathermap.org website via API keys. So after making an account in the website to get access to a free API key used for retrieving data on the weather, I also had to use the website's Geocoding API to get the exact geographic coordinates since for using the website we are required to use these cooredibarte instead of the name of the city.
 
 Challenges faced: At the start I struggled to show the source I was working with in the console even after comparing my ajax calls and the ".then" promise with earlier activities in the week. I later noticed that even though I had copied the examples from the openweathermap documentation, I had to add the "https" to the beginning of my URLs for it to work.
+After this I still struggled a lot with using the URL's and how to add them together when using the ajax calls.
+But I'm not really sure what the error was for in the end, I wasn't too confident on my second ajax call but I don't know if it was because I couldn't get the right timing to work (I had to make the second ajax run only after the first had given the geographic coordinates so I tried using an "if" statement or putting the second ajax into another "then" promise but neither worked), or if I just didn't write the url correctly (even though I made many attempts at changing the wording)
+
+website url:
+
+
+sreenshot:
+<img src="./images/Screenshot 2023-01-03 at 01-08-25 Weather Dashboard.png" />
--- assets/script.js
+++ assets/script.js
@@ -3,35 +3,36 @@
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b1251da55c6b466dba642669f628fd34
 
 var API_KEY = 'appid=b1251da55c6b466dba642669f628fd34';
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Bujumbura,Burundi&appid=" + API_KEY;
 
 var forcastBaseUrl = 'https://api.openweathermap.org/data/2.5/forecast?';
 
 var geoBaseUrl = 'https://api.openweathermap.org/geo/1.0/direct?';
+
 // city variable is user input:
 // var searchString = "";
 var city = 'New York';
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// })
-//     .then(function (response) {
-//         console.log(queryURL);
-//         console.log(response);
-//         $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-//         $(".wind").text("Wind Speed: " + response.wind.speed);
-//         $(".humidity").text("Humidity: " + response.main.humidity);
-//         console.log("Wind Speed: " + response.wind.speed);
-//         console.log("Humidity: " + response.main.humidity);
-//     });
+var check = 0
 
 $.ajax({
-    url: `${geoBaseUrl}q=${city}&limit5&${API_KEY}`,
+    url: `${geoBaseUrl}q=${city}&limit=1&${API_KEY}`,
     type: "GET"
-}).then(function (response) {
-    console.log(response);
+})
+    .then(function (response) {
+        console.log(response);
+        var lat = console.log(response[0].lat);
+        var lon = console.log(response[0].lon);
+        check=++
+    })
+
+    $.ajax({
+        url: `${forcastBaseUrl}${lat}&${lon}&${API_KEY}`,
+        type: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+    })
+
 
 
     //     var trEl = $('<tr>');
@@ -39,20 +40,15 @@ $.ajax({
     // var lat = .text(response.lat);
     // var lon = console.log(response.lon);
 
-});
 
 
 
 
+// search button:
+// $("#search-button").on("click", function() {
 
-$.ajax({
-    url: `${forcastBaseUrl}&lat=51.507218&lon=-0.127586&&${API_KEY}`,
-    type: "GET"
-}).then(function (response) {
-    console.log(response);
-
-    console.log(response.city.name);
-});
+// }
+// );
 
 
 
